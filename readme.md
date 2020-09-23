7b9a3581d2b2ded8d17bb745a4e7c8597634b9a1
MikeLev1948 <mlevenson@verstaan.com>
2020-09-23 06:20:52 +0300

    readme

---
+++ readme.md
@@ -0,0 +1,32 @@
+# Dealtale api
+
+## description
+
+### **_measure the correlation/importance of relationship between each `metric X` and `target Y ` for one-hot encoded dataset (0,1)_** using
+
+- **Decision Tree classifier feature importance** score `importance` AKA gini importance
+- **Phi coefficient** aka Matthews correlation coefficient `phi_correlation`
+- **Point biserial** correlation `biserial_correlation`
+- **P-value** `p_value` (aka statistical importance)
+- **Predictive power score** `pred_power` (similar to gini importance measure)
+
+### **_train on dataset and get prediction probablities on unseen one-hot encoded dataset with support vector machine_**
+
+## Installation
+
+- Python == `python 3.7`
+
+### from root of proj dir
+
+- install virtual env
+  - `$ virtualenv myenv`
+- activate virtual env
+- `$ source myenv/bin/activate`
+- install packages in virtual env
+- `$ pip install -r requirements.txt`
+- for local running:
+  - from root of project dir `$ sh run.sh`
+
+## Documentation
+
+once server started go to `/redoc` for redoc documentation or `/docs` for interactive swagger ui documentation
--- static/index.js
+++ static/index.js
@@ -10,37 +10,37 @@ const {
   SvgIcon,
   Link,
   Button,
-  TextField
+  TextField,
 } = MaterialUI;
 
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: "#556cd6",
     },
     secondary: {
-      main: '#19857b',
+      main: "#19857b",
     },
     error: {
       main: colors.red.A400,
     },
     background: {
-      default: '#fff',
+      default: "#fff",
     },
   },
 });
 
-
 function App() {
   return (
     <Container maxWidth="sm">
-      <div style={{ marginTop: 24, }}>
+      <div style={{ marginTop: 24 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Logged In</Typography>
-          <TextField/>
-          <Button> Test </Button>
-          <Link href="/login"> link</Link>
+          Logged In
+        </Typography>
+        <TextField />
+        <Button> Test </Button>
+        <Link href="/login"> link</Link>
       </div>
     </Container>
   );
@@ -51,5 +51,5 @@ ReactDOM.render(
     <CssBaseline />
     <App />
   </ThemeProvider>,
-  document.querySelector('#root'),
+  document.querySelector("#root")
 );
